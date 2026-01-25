f92807dc3cf15f0b4833ac39f825392890c2f518
Giovanni Muniz <tul40930@temple.edu>
2026-01-24 21:06:40 -0500

    added packages for user authentication

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
 app.listen(port, () => { 
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+console.log("App is set up and ready to go!");
+
